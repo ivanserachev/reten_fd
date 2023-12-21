ef014caeb01804912aef7341c1be6d6d1e6227bb
Ivan Serachev <admin@admin.com>
2023-12-21 18:01:07 +0300

    changed features

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import polars as pl
 import numpy as np
 from sklearn.model_selection import train_test_split
 from catboost import CatBoostClassifier
-from typing import TypedDict
+from typing import TypedDict, List
 from numpy.typing import NDArray
 
 LearningData = TypedDict('LearningData', {'x_train': NDArray,
                                           'x_test': NDArray,
                                           'y_train': NDArray,
                                           'y_test': NDArray,
-                                          'cat_features': NDArray})
+                                          'cat_features': List})
 
 
 def execute_data() -> pl.DataFrame:
@@ -25,60 +25,59 @@ def transform_data(data: pl.DataFrame) -> LearningData:
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=1234)
 
-    cat_features = x_train.select(pl.col(['GENDER',
-                                          'MARITAL_STATUS',
-                                          'EMPLOYMENT_TYPE',
-                                          'DEPENDENTS',
-                                          'EDUCATION',
-                                          'EMAIL_HOST',
-                                          'NAME',
-                                          'MIDDLENAME',
-                                          'WORK_PLACE_FEDERAL_DISTRICT',
-                                          'WORK_PLACE_REGION',
-                                          'WORK_PLACE_REGION_TYPE_FULL',
-                                          'WORK_PLACE_CITY',
-                                          'WORK_PLACE_CITY_TYPE_FULL',
-                                          'WORK_PLACE_CITY_DISTRICT_WITH_TYPE',
-                                          'WORK_PLACE_STEAD_TYPE_FULL',
-                                          'WORK_PLACE_HOUSE_TYPE_FULL',
-                                          'WORK_PLACE_CAPITAL_MARKER',
-                                          'WORK_PLACE_FIAS_LEVEL',
-                                          'WORK_PLACE_TIMEZONE',
-                                          'WORK_PLACE_OKVED',
-                                          'WORK_PLACE_OKVED_TYPE',
-                                          'WORK_PLACE_OKATO',
-                                          'WORK_PLACE_OKFS',
-                                          'WORK_PLACE_OKOGU',
-                                          'WORK_PLACE_OKPO',
-                                          'WORK_PLACE_OKTMO',
-                                          'WORK_PLACE_NAME',
-                                          'WORK_PLACE_OPF_FULL',
-                                          'WORK_PLACE_FINANCE_YEAR',
-                                          'PASSPORTISSUERCODE',
-                                          'PASSPORTSERIES',
-                                          'REGISTRATIONADDRESS_FEDERAL_DISTRICT',
-                                          'REGISTRATIONADDRESS_REGION',
-                                          'REGISTRATIONADDRESS_REGION_TYPE_FULL',
-                                          'REGISTRATIONADDRESS_CITY',
-                                          'REGISTRATIONADDRESS_CITY_TYPE_FULL',
-                                          'REGISTRATIONADDRESS_CITY_DISTRICT_WITH_TYPE',
-                                          'REGISTRATIONADDRESS_STEAD_TYPE_FULL',
-                                          'REGISTRATIONADDRESS_HOUSE_TYPE_FULL',
-                                          'REGISTRATIONADDRESS_FIAS_LEVEL',
-                                          'REGISTRATIONADDRESS_TIMEZONE',
-                                          'RESIDENTIALADDRESS_FEDERAL_DISTRICT',
-                                          'RESIDENTIALADDRESS_REGION',
-                                          'RESIDENTIALADDRESS_REGION_TYPE_FULL',
-                                          'RESIDENTIALADDRESS_CITY',
-                                          'RESIDENTIALADDRESS_CITY_TYPE_FULL',
-                                          'RESIDENTIALADDRESS_CITY_DISTRICT_WITH_TYPE',
-                                          'RESIDENTIALADDRESS_STEAD_TYPE_FULL',
-                                          'RESIDENTIALADDRESS_HOUSE_TYPE_FULL',
-                                          'RESIDENTIALADDRESS_CAPITAL_MARKER',
-                                          'RESIDENTIALADDRESS_FIAS_LEVEL',
-                                          'RESIDENTIALADDRESS_TIMEZONE'])
-                                  )
-    cat_features = cat_features.to_numpy()
+    cat_features = [2,3,5,6,7,9,10,11,12,13,14,15,16,17,18,19,20,21,22,24,25,26,27,28,29,30,31,32,33,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61]
+    # text_features = ['GENDER',
+    #                  'MARITAL_STATUS',
+    #                  'EMPLOYMENT_TYPE',
+    #                  'DEPENDENTS',
+    #                  'EDUCATION',
+    #                  'EMAIL_HOST',
+    #                  'NAME',
+    #                  'MIDDLENAME',
+    #                  'WORK_PLACE_FEDERAL_DISTRICT',
+    #                  'WORK_PLACE_REGION',
+    #                  'WORK_PLACE_REGION_TYPE_FULL',
+    #                  'WORK_PLACE_CITY',
+    #                  'WORK_PLACE_CITY_TYPE_FULL',
+    #                  'WORK_PLACE_CITY_DISTRICT_WITH_TYPE',
+    #                  'WORK_PLACE_STEAD_TYPE_FULL',
+    #                  'WORK_PLACE_HOUSE_TYPE_FULL',
+    #                  'WORK_PLACE_CAPITAL_MARKER',
+    #                  'WORK_PLACE_FIAS_LEVEL',
+    #                  'WORK_PLACE_TIMEZONE',
+    #                  'WORK_PLACE_OKVED',
+    #                  'WORK_PLACE_OKVED_TYPE',
+    #                  'WORK_PLACE_OKATO',
+    #                  'WORK_PLACE_OKFS',
+    #                  'WORK_PLACE_OKOGU',
+    #                  'WORK_PLACE_OKPO',
+    #                  'WORK_PLACE_OKTMO',
+    #                  'WORK_PLACE_NAME',
+    #                  'WORK_PLACE_OPF_FULL',
+    #                  'WORK_PLACE_FINANCE_YEAR',
+    #                  'PASSPORTISSUERCODE',
+    #                  'PASSPORTSERIES',
+    #                  'REGISTRATIONADDRESS_FEDERAL_DISTRICT',
+    #                  'REGISTRATIONADDRESS_REGION',
+    #                  'REGISTRATIONADDRESS_REGION_TYPE_FULL',
+    #                  'REGISTRATIONADDRESS_CITY',
+    #                  'REGISTRATIONADDRESS_CITY_TYPE_FULL',
+    #                  'REGISTRATIONADDRESS_CITY_DISTRICT_WITH_TYPE',
+    #                  'REGISTRATIONADDRESS_STEAD_TYPE_FULL',
+    #                  'REGISTRATIONADDRESS_HOUSE_TYPE_FULL',
+    #                  'REGISTRATIONADDRESS_FIAS_LEVEL',
+    #                  'REGISTRATIONADDRESS_TIMEZONE',
+    #                  'RESIDENTIALADDRESS_FEDERAL_DISTRICT',
+    #                  'RESIDENTIALADDRESS_REGION',
+    #                  'RESIDENTIALADDRESS_REGION_TYPE_FULL',
+    #                  'RESIDENTIALADDRESS_CITY',
+    #                  'RESIDENTIALADDRESS_CITY_TYPE_FULL',
+    #                  'RESIDENTIALADDRESS_CITY_DISTRICT_WITH_TYPE',
+    #                  'RESIDENTIALADDRESS_STEAD_TYPE_FULL',
+    #                  'RESIDENTIALADDRESS_HOUSE_TYPE_FULL',
+    #                  'RESIDENTIALADDRESS_CAPITAL_MARKER',
+    #                  'RESIDENTIALADDRESS_FIAS_LEVEL',
+    #                  'RESIDENTIALADDRESS_TIMEZONE']
     x_train = x_train.to_numpy()
     x_test = x_test.to_numpy()
     y_train = y_train.to_numpy()
@@ -110,7 +109,7 @@ def learning(learn_data: LearningData) -> None:
 
     model.fit(
         learn_data['x_train'], learn_data['y_train'],
-        cat_features=learn_data['cat_features'],
+        text_features=learn_data['cat_features'],
         eval_set=(learn_data['x_test'], learn_data['y_test']),
         logging_level='Info',
         plot=True
@@ -122,5 +121,6 @@ def flow():
     data = execute_data()
     transformed_data = transform_data(data=data)
     learning(learn_data=transformed_data)
+
 
 flow()
