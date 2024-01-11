7d859e38949ceab3b011de8e67d532a7f2738c8a
Ivan Serachev <admin@admin.com>
2024-01-11 16:35:38 +0300

    add plot

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flow import *
 import polars as pl
 import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
-from catboost import CatBoostClassifier
+from catboost import CatBoostClassifier, Pool
+from catboost import utils
 from typing import TypedDict, List
 from numpy.typing import NDArray
+import matplotlib.pyplot as plt
 
 LearningData = TypedDict('LearningData', {'x_train': NDArray,
                                           'x_test': NDArray,
@@ -20,73 +23,27 @@ def execute_data() -> pl.DataFrame:
 
 
 def transform_data(data: pl.DataFrame) -> LearningData:
-    data.fill_null('')
-    data.fill_nan('')
-    x = data.select(pl.exclude(columns=['SALES']))
-    y = data.select('SALES')
+    data = data.to_pandas()
+    data = data.astype('string')
+    print(data.dtypes)
+    # data =data.fill_null('')
+    data =data.fillna('')
+    x = data.loc[:, data.columns!='SALES']
+    # x = data.select(pl.exclude(columns=['SALES']))
+    y = data['SALES']
+    # y = data.select('SALES')
 
-    with pl.Config(tbl_cols=x.width):
-        print(x)
+    # with pl.Config(tbl_cols=x.width):
+    #     print(x)
+    pd.set_option('display.max_columns', None)
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=1234)
 
-    cat_features = [2,3,5,6,7,9,10,11,12,13,14,15,16,17,18,19,20,21,22,24,25,26,27,28,29,30,31,32,33,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61]
-    # text_features = ['GENDER',
-    #                  'MARITAL_STATUS',
-    #                  'EMPLOYMENT_TYPE',
-    #                  'DEPENDENTS',
-    #                  'EDUCATION',
-    #                  'EMAIL_HOST',
-    #                  'NAME',
-    #                  'MIDDLENAME',
-    #                  'WORK_PLACE_FEDERAL_DISTRICT',
-    #                  'WORK_PLACE_REGION',
-    #                  'WORK_PLACE_REGION_TYPE_FULL',
-    #                  'WORK_PLACE_CITY',
-    #                  'WORK_PLACE_CITY_TYPE_FULL',
-    #                  'WORK_PLACE_CITY_DISTRICT_WITH_TYPE',
-    #                  'WORK_PLACE_STEAD_TYPE_FULL',
-    #                  'WORK_PLACE_HOUSE_TYPE_FULL',
-    #                  'WORK_PLACE_CAPITAL_MARKER',
-    #                  'WORK_PLACE_FIAS_LEVEL',
-    #                  'WORK_PLACE_TIMEZONE',
-    #                  'WORK_PLACE_OKVED',
-    #                  'WORK_PLACE_OKVED_TYPE',
-    #                  'WORK_PLACE_OKATO',
-    #                  'WORK_PLACE_OKFS',
-    #                  'WORK_PLACE_OKOGU',
-    #                  'WORK_PLACE_OKPO',
-    #                  'WORK_PLACE_OKTMO',
-    #                  'WORK_PLACE_NAME',
-    #                  'WORK_PLACE_OPF_FULL',
-    #                  'WORK_PLACE_FINANCE_YEAR',
-    #                  'PASSPORTISSUERCODE',
-    #                  'PASSPORTSERIES',
-    #                  'REGISTRATIONADDRESS_FEDERAL_DISTRICT',
-    #                  'REGISTRATIONADDRESS_REGION',
-    #                  'REGISTRATIONADDRESS_REGION_TYPE_FULL',
-    #                  'REGISTRATIONADDRESS_CITY',
-    #                  'REGISTRATIONADDRESS_CITY_TYPE_FULL',
-    #                  'REGISTRATIONADDRESS_CITY_DISTRICT_WITH_TYPE',
-    #                  'REGISTRATIONADDRESS_STEAD_TYPE_FULL',
-    #                  'REGISTRATIONADDRESS_HOUSE_TYPE_FULL',
-    #                  'REGISTRATIONADDRESS_FIAS_LEVEL',
-    #                  'REGISTRATIONADDRESS_TIMEZONE',
-    #                  'RESIDENTIALADDRESS_FEDERAL_DISTRICT',
-    #                  'RESIDENTIALADDRESS_REGION',
-    #                  'RESIDENTIALADDRESS_REGION_TYPE_FULL',
-    #                  'RESIDENTIALADDRESS_CITY',
-    #                  'RESIDENTIALADDRESS_CITY_TYPE_FULL',
-    #                  'RESIDENTIALADDRESS_CITY_DISTRICT_WITH_TYPE',
-    #                  'RESIDENTIALADDRESS_STEAD_TYPE_FULL',
-    #                  'RESIDENTIALADDRESS_HOUSE_TYPE_FULL',
-    #                  'RESIDENTIALADDRESS_CAPITAL_MARKER',
-    #                  'RESIDENTIALADDRESS_FIAS_LEVEL',
-    #                  'RESIDENTIALADDRESS_TIMEZONE']
-    x_train = x_train.to_numpy()
-    x_test = x_test.to_numpy()
-    y_train = y_train.to_numpy()
-    y_test = y_test.to_numpy()
+    cat_features = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66]
+    # x_train = x_train.to_numpy()
+    # x_test = x_test.to_numpy()
+    # y_train = y_train.to_numpy()
+    # y_test = y_test.to_numpy()
     print('data transformed')
     return LearningData(x_train=x_train,
                         x_test=x_test,
@@ -103,8 +60,6 @@ def learning(learn_data: LearningData) -> None:
         iterations=500,
         l2_leaf_reg=4.0,
         learning_rate=0.07521709965938336,
-        save_snapshot=True,
-        snapshot_file='snapshot_best.bkp',
         random_seed=63,
         od_type='Iter',
         od_wait=20,
@@ -120,12 +75,22 @@ def learning(learn_data: LearningData) -> None:
         plot=True
     )
     print('Resulting tree count:', model.tree_count_)
+    # train_pool = Pool(learn_data['x_test'], learn_data['y_test'])
+    # (fpr, tpr, thresholds) = utils.get_roc_curve(model=model, data=train_pool, plot=True)
+    print(model.get_best_score())
+
+    feature_importance = model.feature_importances_
+    sorted_idx = np.argsort(feature_importance)
+    fig = plt.figure(figsize=(12, 6))
+    plt.barh(range(len(sorted_idx)), feature_importance[sorted_idx], align='center')
+    plt.yticks(range(len(sorted_idx)), np.array(learn_data['x_test'].columns)[sorted_idx])
+    plt.title('Feature Importance')
+    plt.show()
 
 
 def flow():
     data = execute_data()
     transformed_data = transform_data(data=data)
     learning(learn_data=transformed_data)
-
 
 flow()
